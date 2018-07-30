a65699f6962aa059377938ba1586127ae48bffcf
Harry V. Torres <torresharry2@gmail.com>
2018-07-30 05:14:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if(string == string.toLowerCase()){
+  if(string == "hello".toLowerCase()){
     
     return console.log("I can\'t hear you!")
     
